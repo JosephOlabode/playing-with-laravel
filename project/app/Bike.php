b9a0004c2a736492ddde98869c0db9bf5b99f2f2
JosephOlabode <josepholabode17@gmail.com>
2020-06-09 03:58:51 +0100

    first commit

---
+++ project/app/Bike.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bike extends Model
+{
+    protected $fillable = [
+        'make', 'model', 'year', 'mods',
+        'picture'
+    ];
+}
